74c135cc19982b1a4b405b5922cd69688a5d83f7
rayala3314 <rayala19@csub.edu>
2020-05-04 22:00:08 -0700

    Recursive function( Deleting Nodes )

---
+++ Lab12/lab12.cpp
@@ -0,0 +1,167 @@
+#include <iostream>
+
+using namespace std;
+
+struct Location{
+    string name;
+    string address;
+ 
+};
+
+struct VisitNode{
+    Location loc;
+    VisitNode *next;
+
+
+};
+
+class Stack{
+    private:
+        VisitNode *head;
+
+        VisitNode *create(){
+            VisitNode *newnode;
+            try{
+                newnode = new VisitNode;
+
+            }
+            catch (bad_alloc){
+                newnode = NULL;
+
+            }
+                
+            return newnode;
+
+        }
+
+        void deallocate(VisitNode *node){
+            
+            //Base condition
+            if( node != NULL ){
+                // deleting node list recursively
+                deallocate( node->next);
+                delete node;
+            }
+
+            //No Loops
+            /*
+            while( hCopy != NULL ){
+                head = head->next;
+                delete hCopy;
+                hCopy = head;
+                
+            }
+         */
+
+        }
+    public:
+        bool push(string name, string address){
+            VisitNode *newnode = create();
+            
+
+            bool successful = false;
+            newnode->loc.name = name;
+            newnode->loc.address = address;
+
+            if( head == NULL){
+                newnode->next = head;
+                head = newnode;
+                newnode->next = NULL;
+                successful = true;
+
+            }
+            else{
+                newnode->next = head;
+                head = newnode;;
+
+
+
+            }
+
+            return successful;
+            
+        }
+        
+        string pop(){
+            VisitNode *curr;
+            string name;
+            
+            if( head != NULL){
+                curr = head;
+                name = curr->loc.name;
+                head = head->next;
+                delete curr;
+
+
+            }
+            return name;
+        
+        }
+        friend void show(Stack &S);
+       
+        ~Stack(){
+            deallocate( head );
+            head = NULL;
+
+        }
+
+
+        
+};
+
+void show(Stack &S){
+    VisitNode *copy;
+    VisitNode *store;
+    copy = S.head;
+    
+    while( copy != NULL ){
+        store = copy->next;
+        cout << copy->loc.name << " ";
+        copy = store;
+
+    } 
+
+
+}
+
+ 
+int main() {
+    Stack browser;
+
+    //Simulating browser history
+    browser.push("Google", "https://google.com");
+    browser.push("Amazon", "https://amazon.com");
+    browser.push("LinkedIn", "https://LinkedIn.com");
+    browser.push("Reddit", "https://reddit.com");
+
+    show(browser);
+
+    //Simulating clicking back button
+    
+    cout << endl; 
+
+
+   /* string top = browser.pop();
+    if (top != "")
+          cout << endl << "Clicked back from " << top << endl;
+    show(browser);
+
+    cout << endl;    
+    //Simulating clicking back button
+    top = browser.pop();
+    if (top != "" )
+        cout << endl << "Clicked back from " << top << endl;
+    show(browser);
+    
+    cout << endl;    
+    //Simulating clicking back button
+    top = browser.pop();
+    if (top != "" )
+        cout << endl << "Clicked back from " << top << endl;
+    show(browser);
+
+    cout << endl << endl;
+*/
+    
+    return 0;
+}
